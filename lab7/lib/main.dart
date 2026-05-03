f565a028bc5d15fc261543f68c13e0c31f62332c
Hai Nguyen <icurff@gmail.com>
2026-05-03 13:31:50 +0700

    fix path

--- lab7/lib/main.dart
+++ lab7/lib/main.dart
@@ -30,7 +30,7 @@ class _StoryPageState extends State<StoryPage> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('/images/background.png'),
+            image: AssetImage('assets/images/background.png'),
             fit: BoxFit.cover,
           ),
         ),
